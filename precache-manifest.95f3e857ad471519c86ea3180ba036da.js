869fc5e7930f6cfa3749bdea4ef977c7acbb83bc
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2020-08-21 21:34:09 -0400

    Updates

---
+++ precache-manifest.95f3e857ad471519c86ea3180ba036da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4455f2a06089de773e872b7d87d992",
+    "url": "/react-chess-game/index.html"
+  },
+  {
+    "revision": "18c7394f1bfd693648fe",
+    "url": "/react-chess-game/static/css/main.799e5438.chunk.css"
+  },
+  {
+    "revision": "278bcd6dc60b6d93e0ca",
+    "url": "/react-chess-game/static/js/2.193aede0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-chess-game/static/js/2.193aede0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18c7394f1bfd693648fe",
+    "url": "/react-chess-game/static/js/main.6af41147.chunk.js"
+  },
+  {
+    "revision": "996e9d609322d04aae2a",
+    "url": "/react-chess-game/static/js/runtime-main.82d42676.js"
+  },
+  {
+    "revision": "f0b3b69c193478f25298cedf3e109d8d",
+    "url": "/react-chess-game/static/media/ChessLogo.f0b3b69c.png"
+  },
+  {
+    "revision": "79b18a5d205cdebc264fc06817b73584",
+    "url": "/react-chess-game/static/media/menu.79b18a5d.png"
+  }
+]);
